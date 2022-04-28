47416b171981a6b4288ba018a329a6b662015870
paul <paul.surrans@epitech.eu>
2022-04-28 11:26:32 +0200

    feat(step-1): taking photos

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+import {UserPhoto} from "./userPhoto";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  photos: UserPhoto[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: 'soon...',
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+}
